bc1c2f85ac7c49a142e9f7adef7f1bd8ba0ba728
Анастасия Филатьева <nastasya.filateva@gmail.com>
2025-05-31 17:25:45 +0300

    laaast_god

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -84,7 +84,11 @@ WSGI_APPLICATION = 'foodgram_backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/data/db.sqlite3',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
 }
 }
 
